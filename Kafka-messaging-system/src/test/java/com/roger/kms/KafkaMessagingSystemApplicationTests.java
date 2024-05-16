a4c43575517209740e2ab1ab33d41f55224b15a4
Rongzheng He <rhe68@gatech.edu>
2024-05-16 17:18:26 -0400

    created parent project

---
+++ Kafka-messaging-system/src/test/java/com/roger/kms/KafkaMessagingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roger.kms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessagingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
