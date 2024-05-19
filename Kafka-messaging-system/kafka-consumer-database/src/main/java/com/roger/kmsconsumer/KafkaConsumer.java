466276aec4895e2e574cfa07add27c212951bada
Rongzheng He <rhe68@gatech.edu>
2024-05-19 00:21:34 -0400

    parse event data and persist it

--- Kafka-messaging-system/kafka-consumer-database/src/main/java/com/roger/kmsconsumer/KafkaConsumer.java
+++ Kafka-messaging-system/kafka-consumer-database/src/main/java/com/roger/kmsconsumer/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.roger.kmsconsumer;
 
 import com.roger.kmsconsumer.db.EventRepository;
 import com.roger.kmsconsumer.db.WikimediaRecord;
+import com.roger.kmsconsumer.model.JsonToPOJO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -19,8 +20,7 @@ public class KafkaConsumer {
     @KafkaListener(topics = "wikimedia-recentchange", groupId = "myGroup")
     public void consume(String eventMessage) {
         LOGGER.info("event message received -> " + eventMessage);
-        WikimediaRecord wikimediaRecord = new WikimediaRecord();
-        wikimediaRecord.setEventMessage(eventMessage.substring(1, 200));
+        WikimediaRecord wikimediaRecord = JsonToPOJO.toEventInfo(eventMessage);
         eventRepository.save(wikimediaRecord);
     }
 }
